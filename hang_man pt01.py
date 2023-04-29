f5859b9c89d93f7639eb48aef52343f84d71de83
snehasish <rsnhasish125@gmail.com>
2023-04-29 15:40:47 +0530

    final fixes

--- hang_man pt01.py
+++ hang_man pt01.py
@@ -1,7 +1,7 @@
 import random
 import sys
 
-word_list = ['appple' , 'bot' ,'cat' , 'python']
+word_list = ['apple' , 'bot' ,'cat' , 'python']
 empty_list = []
 chosen_word = random.choice(word_list).lower()
 
@@ -20,19 +20,17 @@ wrong = '''
 
 for i in chosen_word:
     user_ch =input('Choose a Letter : ').lower()
-    if user_ch in chosen_word:
-#        empty_list += user_ch
-#        empty_list.insert(chosen_word.index(i),user_ch)        
+    if user_ch in chosen_word:      
         empty_list[chosen_word.index(i)] = user_ch
         print(f'{empty_list}{correct} ')
     else:
         print(f'{empty_list} {wrong} ')
         while not user_ch == i:
             user_ch =input('Choose a Letter : ').lower()
-            print(f'{empty_list} {wrong} "Guess again" ')
+            print(f'{empty_list} {wrong} Guess again ')
         else:
              empty_list[chosen_word.index(i)] = user_ch       
-#    print(f'{empty_list}{correct} ')
+             print(f'{empty_list}{correct} ')
 '''
     if not '_' in empty_list:
         sys.exit(0)
